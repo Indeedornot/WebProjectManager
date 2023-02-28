a9ea147bff9011f040fccfa1f34fbfa37fe2ecfd
Indeedornot <gravitybartek@gmail.com>
2023-02-28 18:52:42 +0100

    api: add database

---
+++ api/Controllers/ProjectController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+using shared.Models;
+
+namespace api.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class ProjectController : ControllerBase {
+    // [HttpGet(Name = "GetProject")]
+    // public IEnumerable<Project> GetProject(int id) {
+    //     return new Project();
+    // }
+}
---
+++ api/Database/DataContext.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+
+using shared.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Database {
+    public class DataContext : DbContext {
+        protected readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) {
+            // connect to sqlite database
+            options.UseSqlite(Configuration.GetConnectionString("ProjectDatabase"));
+        }
+
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+            builder.Entity<User>()
+                .HasMany(u => u.Projects)
+                .WithMany(p => p.Assignees)
+                .UsingEntity(j => j.ToTable("UserProject"));
+        }
+    }
+}
