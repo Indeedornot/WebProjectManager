67684245482707883706a07e0a0b1fa605cb9a9d
Indeedornot <gravitybartek@gmail.com>
2023-02-28 22:40:19 +0100

    shared: rename NewProject to ProjectDTO and add explicit cast operator

--- shared/Models/Project.cs
+++ shared/Models/Project.cs
@@ -14,11 +14,21 @@ namespace shared.Models {
         public IEnumerable<User> Assignees { get; set; }
     }
 
-    public class NewProject {
+    public class ProjectDTO {
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime DueDate { get; set; }
         public string Status { get; set; }
         public IEnumerable<User> Assignees { get; set; }
+
+        public static explicit operator Project(ProjectDTO projectDTO) {
+            return new Project {
+                Name = projectDTO.Name,
+                Description = projectDTO.Description,
+                DueDate = projectDTO.DueDate,
+                Status = projectDTO.Status,
+                Assignees = projectDTO.Assignees
+            };
+        }
     }
 }
