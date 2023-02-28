0e1f73317835fc4e8a5a848b3d644c8e2f979da1
Indeedornot <gravitybartek@gmail.com>
2023-02-28 18:54:41 +0100

    api: add databasecontext to DI

--- api/Program.cs
+++ api/Program.cs
@@ -1,8 +1,13 @@
+using api.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DataContext>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
