48046f66149501bab0814dcd89878e42b5bc88be
Indeedornot <gravitybartek@gmail.com>
2023-03-04 15:19:29 +0100

    client: use blazor component css isolation

--- client/Program.cs
+++ client/Program.cs
@@ -5,6 +5,7 @@ using Tailwind;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddServerSideBlazor();
+builder.WebHost.UseStaticWebAssets();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
