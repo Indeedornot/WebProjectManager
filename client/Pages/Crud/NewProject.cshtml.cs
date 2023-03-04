a58dcc27aa51e4d9de0ef6310516dcd577f30b63
Indeedornot <gravitybartek@gmail.com>
2023-03-04 20:51:46 +0100

    client: add new project route and component

---
+++ client/Pages/Crud/NewProject.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace client.Pages {
+    public class AddProjectModel : PageModel {
+        public void OnGet() {
+        }
+    }
+}
