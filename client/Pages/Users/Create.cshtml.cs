4d03a25fd46741883624172d346e0da0b8f0f571
Indeedornot <gravitybartek@gmail.com>
2023-03-06 19:01:54 +0100

    feat(client): add user create route

---
+++ client/Pages/Users/Create.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace client.Pages.Users;
+
+public class CreateModel : PageModel {
+    public void OnGet() {
+    }
+}
