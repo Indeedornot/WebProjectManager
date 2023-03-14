8c3b4adf976abb9e6b1b2f1223788a1be4a5f815
Indeedornot <gravitybartek@gmail.com>
2023-03-14 17:01:44 +0100

    remove client project

--- client/Pages/Projects/create.cshtml.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace client.Pages.Projects;
-public class CreateModel : PageModel {
-    public void OnGet() {
-    }
-}
--- client/Pages/Users/Create.cshtml.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace client.Pages.Users;
-
-public class CreateModel : PageModel {
-    public void OnGet() {
-    }
-}
