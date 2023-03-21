5171bc777bd40d1d6ce8b4e1306b791261753fe1
Indeedornot <gravitybartek@gmail.com>
2023-03-21 18:38:21 +0100

    feat: export api routes

--- shared/Common/Routes.cs
+++ shared/Common/Routes.cs
@@ -2,4 +2,22 @@
 
 public class Routes {
     public const string Hello = $"/project/hello";
+
+    public static class Project {
+        public const string Create = "/project/create";
+        public const string Delete = "/project/delete/{id}";
+        public const string Update = "/project/update";
+
+        public const string Get = "/project/{id}";
+        public const string GetAll = "/projects/{page}/{pageSize}";
+
+        public const string Leave = "/project/leave/{id}";
+        public const string Join = "/project/join/{id}";
+    }
+
+    public static class User {
+        public const string Get = "/user/{id}";
+        public const string GetAll = "/users/{page}/{pageSize}";
+        public const string GetProjects = "/user/projects/{id}";
+    }
 }
