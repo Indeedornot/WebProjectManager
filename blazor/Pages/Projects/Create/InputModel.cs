ec0d76fb8bcd038d9101c8499c0533538d51a1af
Indeedornot <gravitybartek@gmail.com>
2023-03-27 23:32:29 +0200

    feat(blazor): Improve project creation ui

---
+++ blazor/Pages/Projects/Create/InputModel.cs
@@ -0,0 +1,15 @@
+﻿using blazor.Validators;
+
+using shared.Models;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace blazor.Pages.Projects.Create;
+
+public class InputModel {
+    [Required] public string Name { get; set; } = string.Empty;
+    [Required] public string Description { get; set; } = string.Empty;
+    [LaterThanNow] public DateTime DueDate { get; set; } = DateTime.Now;
+    [Required] public string Status { get; set; } = string.Empty;
+    [MinLength(1)] public IEnumerable<string> Assignees { get; set; } = Array.Empty<string>();
+}
---
+++ blazor/Validators/LaterThanNowAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace blazor.Validators;
+
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+public class LaterThanNowAttribute : ValidationAttribute {
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext) {
+        bool isDate = DateTime.TryParse(value?.ToString(), out DateTime result);
+        if (!isDate) {
+            throw new ArgumentException("Value is not a date");
+        }
+
+        if (result <= DateTime.Now) {
+            return new ValidationResult("Date should be later than now");
+        }
+
+        return null;
+    }
+}
